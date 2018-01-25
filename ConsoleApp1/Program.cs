06c1c359b2e50ae9676032f3135175e2854daf60
Felix Freiberger <felix@fefrei.de>
2018-01-25 14:29:45 +0100

    Initial commit.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        public const string name = "June"; // this should be an error
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
